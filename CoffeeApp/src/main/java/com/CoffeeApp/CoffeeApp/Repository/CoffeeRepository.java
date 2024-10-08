1759800be23a17cbe815b73d04d06d7d6c5a658b
tanmaygolokait <tanmay@golokait.com>
2024-10-08 17:32:07 +0530

    Revert "Created REST APIs, implemented Service, Repository, Entity and controllers. Yet to connect repository to database"
    This reverts commit 63cb098eedfd05c7a2f67e94ed93d9cb46b8b46b.

--- CoffeeApp/src/main/java/com/CoffeeApp/CoffeeApp/Repository/CoffeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.CoffeeApp.CoffeeApp.Repository;
-
-import com.CoffeeApp.CoffeeApp.Entity.CoffeeAppEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CoffeeRepository extends MongoRepository<CoffeeAppEntity,String> {
-}
